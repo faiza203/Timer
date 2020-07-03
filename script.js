3d0c08603e73d3f138a6bff3171f1d706b7f9720
faiza203 <60883181+faiza203@users.noreply.github.com>
2020-07-03 11:42:46 +0500

    insert pause function

--- script.js
+++ script.js
@@ -2,18 +2,27 @@ const time = document.getElementById("time-input");
 const play = document.getElementById("play-button");
 const pause = document.getElementById("pause-button");
 class Timer {
-  constructor(time, play, pause) {
-    this.time = time;
-    this.play = play;
-    this.pause = pause;
-    this.play.addEventListener("click", this.start);
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+    this.startButton.addEventListener("click", this.start);
+    this.pauseButton.addEventListener("click", this.pause);
   }
-  start() {
-    this.startInterval = setInterval(function () {
-      time.value = time.value - 1;
-      console.log(time.value);
-    },1000);
-  }
-
+  start = () => {
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+  };
+  pause = () => {
+    clearInterval(this.interval);
+  };
+  tick = () => {
+    const timeRemaining = parseFloat(this.durationInput.value);
+    this.durationInput.value = timeRemaining - 1;
+    console.log(this.durationInput.value);
+    if (this.durationInput.value <= 0) {
+      this.pause();
+    }
+  };
 }
 const timer = new Timer(time, play, pause);
